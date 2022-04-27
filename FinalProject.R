5c22692d2727bcf6c88329e667a6f42da5f44c8d
hawkinsnick <nhawkins@hamilton.edu>
2022-04-27 17:24:13 +0000

    worked more project

--- FinalProject.R
+++ FinalProject.R
@@ -1,5 +1,20 @@
 # Final Project
 
+# Installing packages
+# Comment oackages out after uploading them
+install.packages(c("dplyr", "lubridate", "usmap", "ggplot2"))
+
+# Bringing packages into library
+library(usmap) 
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+
+
+plot_usmap(regions = "states") + 
+  labs(title = "Map of United States",
+       subtitle = "Renewable Electricity Generation Over Time") + 
+  theme(panel.background=element_blank())
 
 
 
