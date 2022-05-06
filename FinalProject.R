f1a74d159a5d94f15d005fc8fc9a0e30cab2c025
hawkinsnick <nhawkins@hamilton.edu>
2022-05-06 10:51:20 +0000

    cleaned up code

--- FinalProject.R
+++ FinalProject.R
@@ -8,8 +8,6 @@ install.packages("maps")
 install.packages("mapdata")
 install.packages("sf")
 install.packages("tmap")
-install.packages("classInt")
-install.packages("RColorBrewer")
 
 
 # Bringing packages into library
@@ -23,8 +21,7 @@ library(maps)
 library(mapdata)
 library(sf)
 library(tmap)
-library(classInt)
-library(RColorBrewer)
+
 
 # Reading in U.S. Sources of Electricity (all in TWh)
 US_Sources <- read.csv("/cloud/project/Sources_FinalDataset.csv")
@@ -54,7 +51,7 @@ ElectrictyMix_GRAPH <- ggplot(data = DataPlot_USEnergy, # data for plot
   geom_line(size = .75)+
   scale_y_continuous(labels = comma)+ 
   labs(x="Year", y="Electricty Production (in TWh)")+ 
-  ggtitle("United States Electricity Production by Source")+
+  ggtitle("United States Electricity Production by Low-Carbon Source")+
   theme_classic()
 ElectrictyMix_GRAPH
 
@@ -87,80 +84,42 @@ Sources_2021 <- read.csv("/cloud/project/2021_DATA.csv")
   # Hydro = HPS & HYC
   # Other & Biomass = ORW, OTH, WWW, GEO
 
-# 2001 - Filtering for fuel codes that I want
+# 2001 - Filtering for fuel code that I want to analyze
 Filter_2001 <- Sources_2001 %>% # data frame with pipe
   group_by(State) %>% # group data frame by state
-  filter(AER.Fuel.Type.Code == "NUC" |
-           AER.Fuel.Type.Code == "WND" |
-           AER.Fuel.Type.Code == "HPS" |
-           AER.Fuel.Type.Code == "HYC" |
-           AER.Fuel.Type.Code == "ORW" |
-           AER.Fuel.Type.Code == "OTH" |
-           AER.Fuel.Type.Code == "WWW" |
-           AER.Fuel.Type.Code == "GEO") # filtering for fuel codes
+  filter(AER.Fuel.Type.Code == "WND" ) # filtering for fuel wind
 # Converting column to numeric
 Filter_2001$Net.Generation.MWh <- as.numeric(as.character(Filter_2001$Net.Generation.MWh))
 
 
-# 2006 - Filtering for fuel codes that I want
+# 2006 - Filtering for fuel code that I want
 Filter_2006 <- Sources_2006 %>% # data frame with pipe
   group_by(State) %>% # group data frame by state
-  filter(AER.Fuel.Type.Code == "NUC" |
-           AER.Fuel.Type.Code == "WND" |
-           AER.Fuel.Type.Code == "HPS" |
-           AER.Fuel.Type.Code == "HYC" |
-           AER.Fuel.Type.Code == "ORW" |
-           AER.Fuel.Type.Code == "OTH" |
-           AER.Fuel.Type.Code == "WWW" |
-           AER.Fuel.Type.Code == "GEO") # filtering for fuel codes
+  filter(AER.Fuel.Type.Code == "WND") # filtering for wind
 # Converting column to numeric
 Filter_2006$Net.Generation.MWh <- as.numeric(as.character(Filter_2006$Net.Generation.MWh))
 
 
-
-# 2011 - Filtering for fuel codes that I want
+# 2011 - Filtering for fuel code that I want
 Filter_2011 <- Sources_2011 %>% # data frame with pipe
   group_by(State) %>% # group data frame by state
-  filter(AER.Fuel.Type.Code == "NUC" |
-           AER.Fuel.Type.Code == "WND" |
-           AER.Fuel.Type.Code == "HPS" |
-           AER.Fuel.Type.Code == "HYC" |
-           AER.Fuel.Type.Code == "ORW" |
-           AER.Fuel.Type.Code == "OTH" |
-           AER.Fuel.Type.Code == "WWW" |
-           AER.Fuel.Type.Code == "GEO") # filtering for fuel codes 
+  filter(AER.Fuel.Type.Code == "WND") # filtering for wind
 # Converting column to numeric
 Filter_2011$Net.Generation.MWh <- as.numeric(Filter_2011$Net.Generation.MWh)
 
 
-
-# 2016 - Filtering for fuel codes that I want
+# 2016 - Filtering for fuel code that I want
 Filter_2016 <- Sources_2016 %>% # data frame with pipe
   group_by(State) %>% # group data frame by state
-  filter(AER.Fuel.Type.Code == "NUC" |
-           AER.Fuel.Type.Code == "WND" |
-           AER.Fuel.Type.Code == "HPS" |
-           AER.Fuel.Type.Code == "HYC" |
-           AER.Fuel.Type.Code == "ORW" |
-           AER.Fuel.Type.Code == "OTH" |
-           AER.Fuel.Type.Code == "WWW" |
-           AER.Fuel.Type.Code == "GEO") # filtering for fuel codes
+  filter(AER.Fuel.Type.Code == "WND") # filtering for wind
 # Converting column to numeric
 Filter_2016$Net.Generation.MWh <- as.numeric(as.character(Filter_2016$Net.Generation.MWh))
 
 
-
-# 2021 - Filtering for fuel codes that I want
+# 2021 - Filtering for fuel code that I want
 Filter_2021 <- Sources_2021 %>% # data frame with pipe
   group_by(State) %>% # group data frame by state
-  filter(AER.Fuel.Type.Code == "NUC" |
-           AER.Fuel.Type.Code == "WND" |
-           AER.Fuel.Type.Code == "HPS" |
-           AER.Fuel.Type.Code == "HYC" |
-           AER.Fuel.Type.Code == "ORW" |
-           AER.Fuel.Type.Code == "OTH" |
-           AER.Fuel.Type.Code == "WWW" |
-           AER.Fuel.Type.Code == "GEO") # filtering for fuel codes 
+  filter(AER.Fuel.Type.Code == "WND") # filtering for wind 
 # Converting column to numeric
 Filter_2021$Net.Generation.MWh <- as.numeric(as.character(Filter_2021$Net.Generation.MWh))
 
@@ -170,6 +129,7 @@ NetGeneration_2001 <- Filter_2001 %>%
   group_by(Year, State, AER.Fuel.Type.Code) %>% 
   summarise(Net.Generation.MWh = sum(Net.Generation.MWh))
 # Exporting to excel so I can add lat / long coordinates
+# ALSO MAKING SURE EVERY STAT IS LISTED - IMPORTANT FOR MAPPING
 write_xlsx(NetGeneration_2001, "\\Client\\C$\\Users\\nickhawkins\\Desktop\\2001.xlsx")
 
 
@@ -178,6 +138,7 @@ NetGeneration_2006 <- Filter_2006 %>%
   group_by(Year, State, AER.Fuel.Type.Code) %>% 
   summarise(Net.Generation.MWh = sum(Net.Generation.MWh))
 # Exporting to excel so I can add lat / long coordinates
+# ALSO MAKING SURE EVERY STAT IS LISTED - IMPORTANT FOR MAPPING
 write_xlsx(NetGeneration_2006, "\\Client\\C$\\Users\\nickhawkins\\Desktop\\2006.xlsx")
 
 
@@ -186,6 +147,7 @@ NetGeneration_2011 <- Filter_2011 %>%
   group_by(Year, State, AER.Fuel.Type.Code) %>% 
   summarise(Net.Generation.MWh = sum(Net.Generation.MWh))
 # Exporting to excel so I can add lat / long coordinates
+# ALSO MAKING SURE EVERY STAT IS LISTED - IMPORTANT FOR MAPPING
 write_xlsx(NetGeneration_2011, "\\Client\\C$\\Users\\nickhawkins\\Desktop\\2011.xlsx")
 
 
@@ -194,6 +156,7 @@ NetGeneration_2016 <- Filter_2016 %>%
   group_by(Year, State, AER.Fuel.Type.Code) %>% 
   summarise(Net.Generation.MWh = sum(Net.Generation.MWh))
 # Exporting to excel so I can add lat / long coordinates
+# ALSO MAKING SURE EVERY STAT IS LISTED - IMPORTANT FOR MAPPING
 write_xlsx(NetGeneration_2016, "\\Client\\C$\\Users\\nickhawkins\\Desktop\\2016.xlsx")
 
 ##### 2021 - Summing Net Generation by State and Fuel Code 
@@ -201,6 +164,7 @@ NetGeneration_2021 <- Filter_2021 %>%
   group_by(Year, State, AER.Fuel.Type.Code) %>% 
   summarise(Net.Generation.MWh = sum(Net.Generation.MWh))
 # Exporting to excel so I can add lat / long coordinates
+# ALSO MAKING SURE EVERY STAT IS LISTED - IMPORTANT FOR MAPPING
 write_xlsx(NetGeneration_2021, "\\Client\\C$\\Users\\nickhawkins\\Desktop\\2021.xlsx")
 
 
@@ -219,6 +183,10 @@ ALL_Data <- list(FINAL_2001, FINAL_2006, FINAL_2011, FINAL_2016, FINAL_2021)
 ALL_DataFrames <- do.call("rbind", ALL_Data)
 # joining dataframes to get state ID to match up
 ID_All <- left_join(stateID, ALL_DataFrames, by=c("STUSPS"="State"))
+
+# Renaming Net.Generation.MWh column
+colnames(ID_All)
+colnames(ID_All)[13] <- "MWh"
 
 
 # Wind 2001 - Creating dataframe for wind and omitting NAs
@@ -242,59 +210,78 @@ WND_2021 <- ID_All[ID_All$AER.Fuel.Type.Code=="WND" & ID_All$Year==2021,]
 New_WND_2021 <- na.omit(WND_2021)
 
 
-# MAKING PLOTS
+# Making Figure #2 - Wind Plots Over Time
 
 # Plot 2001 Wind
-
+tmap_mode("plot")
 tm_shape(New_WND_2001)+
-  tm_polygons("Net.Generation.MWh",
-              n=4,style="jenks",
-              palette = "BuPu", 
-              border.col = "black", 
-              border.alpha = 1)+
-  tm_layout(legend.outside=TRUE)
+  tm_fill("MWh",
+          title="Net Generation (in MWh)",
+          palette = "YlGn",
+          n=4,style="jenks")+ # creates natural breaks
+  tm_borders(col="black", lwd=1)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right",
+            main.title = "2001 Electricity Generation from Wind Energy", 
+            title.size = 1, main.title.position="left")
 
 # Plot 2006 Wind
 tm_shape(New_WND_2006)+
-  tm_polygons("Net.Generation.MWh",
-              n=4,style="jenks",
-              palette = "BuPu", 
-              border.col = "black", 
-              border.alpha = 1)+
-  tm_layout(legend.outside=TRUE)
+  tm_fill("MWh",
+          title="Net Generation (in MWh)",
+          palette = "YlGn",
+          n=4,style="jenks")+ # creates natural breaks
+  tm_borders(col="black", lwd=1)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right",
+            main.title = "2006 Electricity Generation from Wind Energy", 
+            title.size = 1, main.title.position="left")
+
 
 # Plot 2011 Wind
 tm_shape(New_WND_2011)+
-  tm_polygons("Net.Generation.MWh",
-              n=4,style="jenks",
-              palette = "BuPu", 
-              border.col = "black", 
-              border.alpha = 1)+
-  tm_layout(legend.outside=TRUE)
+  tm_fill("MWh",
+          title="Net Generation (in MWh)",
+          palette = "YlGn",
+          n=4,style="jenks")+ # creates natural breaks
+  tm_borders(col="black", lwd=1)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right",
+            main.title = "2011 Electricity Generation from Wind Energy", 
+            title.size = 1, main.title.position="left")
+
 
 # Plot 2016 Wind
 tm_shape(New_WND_2016)+
-  tm_polygons("Net.Generation.MWh",
-              n=4,style="jenks",
-              palette = "BuPu", 
-              border.col = "black", 
-              border.alpha = 1)+
-  tm_layout(legend.outside=TRUE)
-
-# Plot 2021 Wind
-tm_shape(New_WND_2021)+
-  tm_polygons("Net.Generation.MWh",
-              n=4,style="jenks",
-              palette = "BuPu", 
-              border.col = "black", 
-              border.alpha = 1)+
-  tm_layout(legend.outside=TRUE)
+  tm_fill("MWh",
+          title="Net Generation (in MWh)",
+          palette = "YlGn",
+          n=4,style="jenks")+ # creates natural breaks
+  tm_borders(col="black", lwd=1)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right",
+            main.title = "2016 Electricity Generation from Wind Energy", 
+            title.size = 1, main.title.position="left")
 
 
 
 
 # Figure #3
-CO_Resources <- read.csv("/cloud/project/CO_Resources.csv")
+# Plot 2021 Wind
+# PERCENTILE PLOT
+# Creating percentile for breaks in plot
+percent <- c(.01,.25,.5,.75,.99,1)
+quantile(New_WND_2021$MWh, percent)
+break_2021 <- quantile(New_WND_2021$MWh, percent)
+
+
+tm_shape(New_WND_2021)+
+  tm_fill("MWh",
+          title="Net Gen Percentile (MWh)",
+          palette = "YlGn",
+          breaks=break_2021,
+          labels=c("1% - %25", "25% - 50%", "50% - 75%","75% - 99%", "> 99%"))+ # creates natural breaks
+  tm_borders(col="black", lwd=1)+
+  tm_layout(legend.outside = TRUE, legend.outside.position = "right",
+            main.title = "2021 Electricity Generation from Wind Energy", 
+            title.size = 0.5, main.title.position="left")
+
 
 
 
